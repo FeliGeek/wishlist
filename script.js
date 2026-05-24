223c92ec73a50639dfc88b6246999c3e707e6f35
FeliGeek <felipecorreaguzman@gmail.com>
2026-05-24 16:35:11 -0400

    aprendiendo js

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+console.log("Array");
+
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 const ul = document.getElementById("ul");
 
-let listado = [];
+let array = [];
 
-button.addEventListener("click",function(){
-    let texto = input.value;
-    listado.push(texto);
-    mostrarListado();
+button.addEventListener("click", function(){
+    let text = input.value;
+    array.push(text);
+    showText();
 });
 
-function mostrarListado (){
+function showText(){
     ul.innerHTML = "";
-    listado.forEach(function(elemento){
+    array.forEach(function(element, indice){
         let li = document.createElement("li");
-        li.textContent = elemento;
+        li.textContent = element;
+
+        let boton = document.createElement("button");
+        boton.textContent = "❌";
+        boton.addEventListener("click",function(){
+            array.splice(indice, 1);
+            showText();
+        });
+
+        li.appendChild(boton);
         ul.appendChild(li);
     });
 };
